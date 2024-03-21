6b2c1f7047813329281a0973ac1ea21b34d2bdcd
spal4532 <132095001+spal4532@users.noreply.github.com>
2024-03-21 11:22:13 -0400

    Update wind.py

--- nrt_wind/wind.py
+++ nrt_wind/wind.py
@@ -1,20 +1,19 @@
 from ai import cdas
 import pandas as pd
 from datetime import datetime
-import matplotlib.pyplot as plt
 import numpy as np
 # This function returns a dataframe containing Wind/MFI data. Unit of B fields are nT. Unit of `date_time` is UTC
 
 def read_wind_mag(start, end):
-        dataset = 'WI_H0_MFI'
-        vlist = ['BGSE', 'BF1']
-        data = cdas.get_data('sp_phys', dataset, datetime.strptime(str(start)[0:19], "%Y-%m-%d %H:%M:%S"), datetime.strptime(str(end)[0:19], "%Y-%m-%d %H:%M:%S"), variables=vlist)
-        df = pd.DataFrame(data={'date_time':data['EPOCH'],
-                'Bx':data['BX_(GSE)'],
-                'By':data['BY_(GSE)'],
-                'Bz':data['BZ_(GSE)']})
-        df['B'] = np.sqrt(data['BX_(GSE)']**2 + data['BY_(GSE)']**2 + data['BZ_(GSE)']**2)
-        df.set_index('date_time', inplace=True)
-        df.where(df > -1.0e+29, np.nan, inplace=True)
-        df.where(df < 1.0e+29, np.nan, inplace=True)
-        return df
+    dataset = 'WI_H0_MFI'
+    vlist = ['BGSE', 'BF1']
+    data = cdas.get_data('sp_phys', dataset, datetime.strptime(str(start)[0:19], "%Y-%m-%d %H:%M:%S"), datetime.strptime(str(end)[0:19], "%Y-%m-%d %H:%M:%S"), variables=vlist)
+    df = pd.DataFrame(data={'date_time':data['EPOCH'],
+            'Bx':data['BX_(GSE)'],
+            'By':data['BY_(GSE)'],
+            'Bz':data['BZ_(GSE)']})
+    df['B'] = np.sqrt(data['BX_(GSE)']**2 + data['BY_(GSE)']**2 + data['BZ_(GSE)']**2)
+    df.set_index('date_time', inplace=True)
+    df.where(df > -1.0e+29, np.nan, inplace=True)
+    df.where(df < 1.0e+29, np.nan, inplace=True)
+    return df
