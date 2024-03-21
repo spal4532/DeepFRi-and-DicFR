6bdf883d2dc1408aff186d3c9f79512da37372c5
spal4532 <132095001+spal4532@users.noreply.github.com>
2024-03-21 14:17:37 -0400

    Update DeepFRi_test.py

--- nrt_wind/DeepFRi_test.py
+++ nrt_wind/DeepFRi_test.py
@@ -53,7 +53,7 @@ def cnn_model_fit_60_30(imgPath,f):
 
     keras.backend.clear_session()
 
-    savedModel = "teresa_fr_1mres_60mdetrend_nfr_1mres_30mdetrend.h5"
+    savedModel = "fr_1mres_60mdetrend_nfr_1mres_30mdetrend.h5"
     model = get_model1()
     model.load_weights(savedModel)
     # model.summary()
