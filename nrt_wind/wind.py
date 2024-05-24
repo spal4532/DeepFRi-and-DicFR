e46579cdc8eaafe92b932bc8bd8a8784a7f359c5
spal4532 <132095001+spal4532@users.noreply.github.com>
2024-05-24 14:45:24 -0400

    Update wind.py

--- nrt_wind/wind.py
+++ nrt_wind/wind.py
@@ -18,3 +18,17 @@ def read_wind_mag(start, end):
     df.where(df > -1.0e+29, np.nan, inplace=True)
     df.where(df < 1.0e+29, np.nan, inplace=True)
     return df
+def read_wind_mag_GSM(start, end):
+    dataset = 'WI_H0_MFI'
+    vlist = ['BGSM', 'BF1']
+    data = cdas.get_data('sp_phys', dataset, datetime.strptime(str(start)[0:19], "%Y-%m-%d %H:%M:%S"), 
+                         datetime.strptime(str(end)[0:19], "%Y-%m-%d %H:%M:%S"), variables=vlist)
+    df = pd.DataFrame(data={'date_time':data['EPOCH'],
+            'Bx':data['BX_(GSM)'],
+            'By':data['BY_(GSM)'],
+            'Bz':data['BZ_(GSM)']})
+    df['B'] = np.sqrt(data['BX_(GSM)']**2 + data['BY_(GSM)']**2 + data['BZ_(GSM)']**2)
+    df.set_index('date_time', inplace=True)
+    df.where(df > -1.0e+29, np.nan, inplace=True)
+    df.where(df < 1.0e+29, np.nan, inplace=True)
+    return df
